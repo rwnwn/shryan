cc4cc17aa64dcbf233e077bd8404f2bd4468d017
rwnwn <45011018+rwnwn@users.noreply.github.com>
2018-12-02 22:07:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const devs = ["451573626072858624"]; //الايدي حقك
-const adminprefix = ["-"]; // البرفكس
+const adminprefix = ["+"]; // البرفكس
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!devs.includes(message.author.id)) return;
